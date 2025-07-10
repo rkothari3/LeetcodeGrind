102c19b6f91bb5223d534da58b91fdae8b5d4c5b
Rk619504 <rajg6.isb@gmail.com>
2025-07-10 00:10:18 -0400

    LC easy

---
+++ Easy/MeetingRooms.py
@@ -0,0 +1,16 @@
+from typing import List
+class Interval:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1.end > i2.start:
+                return False
+        return True
