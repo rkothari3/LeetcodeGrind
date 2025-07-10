723aab375b78dc319dc562245f9b99b197d1b281
Rk619504 <rajg6.isb@gmail.com>
2025-07-09 23:36:20 -0400

    Yuh

--- Medium/LongestConsecSequence.py
+++ Medium/LongestConsecSequence.py
@@ -16,3 +16,8 @@ class Solution:
                     length += 1
                 longest = max(length, longest)
         return longest
+
+solution = Solution()
+# Example usage:
+print(solution.longestConsecutive([100, 4, 200, 1, 3, 2]))  # Output: 4
+print(solution.longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))  # Output: 9
